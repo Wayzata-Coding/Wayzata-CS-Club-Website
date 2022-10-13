8953b43d833871044eac3479df1150aeb68132ad
PranTanTheMan <Seriousbro23@gmail.com>
2022-10-13 18:18:21 -0500

    Added Meta stuff

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+type MetaLabels = {
+  title: string;
+  lang: string;
+  author: string;
+  description: string;
+  url: string;
+  image: string;
+  tags: string[];
+};
+
+const Meta: MetaLabels = {
+    title: 'WHS CS Club',
+    lang: 'en-us',
+    author: 'Wayzata High School Computer Science Club',
+    description: 'The offcial website for the Wayzata High School computer Science Club. Striving for success in the world of computer science for High School students.',
+    url: '',
+    image: '',
+    tags: [
+        "Wayzata",
+        "High School",
+        "Computer Science",
+        "Club",
+        "WHS",
+        'Wayzata High School'
+    ]
+}
+
+export { Meta };
